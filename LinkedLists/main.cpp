65721efefd10ce0da3e3606b21038785da3d4af8
Satyajit Ghana <satyajitghana7@gmail.com>
2016-09-08 23:09:55 +0530

    Initial Commit

---
+++ LinkedLists/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedLists
+//
+//  Created by Satyajit Ghana on 08/09/16.
+//  Copyright © 2016 Satyajit Ghana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
