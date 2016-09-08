87bd312ade8da29a32e8a09f57830c132a900579
Satyajit Ghana <satyajitghana7@gmail.com>
2016-09-08 23:15:52 +0530

    Linked Lists - Class XII

--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+#include "stacks.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    stacks();
     return 0;
 }
---
+++ LinkedLists/stacks.h
@@ -0,0 +1,72 @@
+//
+//  stacks.h
+//  LinkedLists
+//
+//  Created by Satyajit Ghana on 08/09/16.
+//  Copyright © 2016 Satyajit Ghana. All rights reserved.
+//
+
+#ifndef stacks_h
+#define stacks_h
+
+using namespace std;
+
+struct node {
+    int data;
+    node *next;
+}*top,*save,*newptr,*ptr;
+
+static int no=1;
+
+node* createnode(int i) {
+    ptr= new node;
+    ptr->data=i;
+    ptr->next=NULL;
+    return ptr;
+}
+
+void push(node* ptr) {
+    no++;
+    if (top==NULL) top=ptr;
+    else {
+        save=top;
+        top=ptr;
+        ptr->next=save; }
+}
+
+void pop() {
+    if (top==NULL) { cout<<"\nStack UNDERFLOW !"; exit (-1); }
+    else {
+        ptr=top;
+        top=top->next;
+        delete ptr; }
+}
+
+void display(node* ptr) {
+    while(ptr!=NULL) { cout<<ptr->data<<" "; ptr=ptr->next;} cout<<" END";
+}
+
+int stacks() {
+    top=NULL;
+    int info;
+    char ch='y';
+    while (ch=='y'||ch=='Y') {
+        cout<<"\n\nEnter Data for Node "<<no<<" : "; cin>>info;
+        newptr=createnode(info);
+        if (ptr==NULL) { cout<<"\nError Occured !"; exit (-1); }
+        else push(newptr);
+        cout<<"Want to create more nodes ? (y/n) : "; cin>>ch;
+        cout<<"Your Stack is "; display(top);
+    }cout<<"\n";
+    ch='y';
+    while (ch=='y'||ch=='Y') {
+        cout<<"\nWant to delete a node of your stack ? (y/n) : "; cin>>ch;
+        if (ch=='y'||ch=='Y') {pop();display(top);}
+    }
+    
+    cout<<"Your Stack is : "; display(top);
+    return 0;
+}
+
+
+#endif /* stacks_h */
